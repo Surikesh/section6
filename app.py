89d6cc652d8830a0fa09a35988ecd7798e62deb9
Surikesh <dksurkov@gmail.com>
2020-02-08 20:11:32 +0300

    debug 2nd step

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 db.init_app(app)
-app.run(port=5000, debug=True)
+# app.run(port=5000, debug=True)
 if __name__ == '__main__':
-    from db import db
-    db.init_app(app)
+
+    # db.init_app(app)
     app.run(port=5000, debug=True)
