d8efca9e51d6b4cd4bd0464a7e14f62cb2895872
Surikesh <dksurkov@gmail.com>
2020-02-08 19:38:42 +0300

    Debuging

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
-
+from db import db
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
@@ -23,7 +23,8 @@ api.add_resource(StoreList, '/stores')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
-
+db.init_app(app)
+app.run(port=5000, debug=True)
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
